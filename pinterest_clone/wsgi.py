e15df10642a956c4d6214f9ffe6513188487cca9
zahra <76509412+zahra-Jafari@users.noreply.github.com>
2025-03-18 18:23:31 +0330

    tamamm

--- pinterest_clone/wsgi.py
+++ pinterest_clone/wsgi.py
@@ -1,6 +1,17 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinterest_clone.settings")
 
 application = get_wsgi_application()
+
+
+
+path = '/home/zahrajj/pinterest-clone'
+if path not in sys.path:
+    sys.path.append(path)
+
+
+
+
